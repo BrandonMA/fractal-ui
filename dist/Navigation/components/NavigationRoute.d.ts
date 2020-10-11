0575bcc3932235547ca1623e1f991ed0182fc413
Brandon Maldonado Alonso <brandonma98@protonmail.com>
2020-10-11 17:45:13 -0500

    removed expo app to leave the library isolated

--- dist/Navigation/components/NavigationRoute.d.ts
+++ dist/Navigation/components/NavigationRoute.d.ts
@@ -2,7 +2,7 @@
 import { ScreenProps, StackPresentationTypes } from 'react-native-screens';
 export interface NavigationRouteProps extends Omit<ScreenProps, 'stackPresentation' | 'active'> {
     path: string;
-    children: Array<JSX.Element> | JSX.Element;
+    children?: Array<JSX.Element> | JSX.Element;
     stackPresentation?: StackPresentationTypes;
 }
 export declare function NavigationRoute(props: NavigationRouteProps): JSX.Element;
