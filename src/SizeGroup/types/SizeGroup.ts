2fac7275f1787162a504c052aaabf33cecefac75
BrandonMA <brandonma98@protonmail.com>
2020-12-21 16:38:40 -0600

    moved the module to a different git folder

--- src/SizeGroup/types/SizeGroup.ts
+++
@@ -1,4 +0,0 @@
-import { Size } from './Size';
-import { DeviceType } from 'expo-device';
-
-export type SizeGroup = [Size, DeviceType];
--- src/SizeGroup/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './DimensionsListenerParameter';
-export * from './HeightSizeClass';
-export * from './Size';
-export * from './WidthSizeClass';
-export * from './SizeClass';
-export * from './SizeGroup';
