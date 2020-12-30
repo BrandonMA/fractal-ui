e0c25969b0c04ee4090e17663fada60ad4530286
BrandonMA <brandonma98@protonmail.com>
2020-12-29 19:03:11 -0600

    Fixed shapes to use only integer numbers

--- src/Navigation/components/TabNavigation/TabBar/components/MiddleActionTabBar/MiddleTabBarShape.tsx
+++ src/Navigation/components/TabNavigation/TabBar/components/MiddleActionTabBar/MiddleTabBarShape.tsx
@@ -6,7 +6,7 @@ const LeftShape = memo(
     (props: SvgProps): JSX.Element => {
         return (
             <Svg width={61} height={75} viewBox='0 0 61 75' {...props}>
-                <Path d='M61 75.2H0V0h61c0 4.1-3.1 7.4-7.1 7.9C39.3 10 28 22.5 28 37.7c0 15.2 11.3 27.7 25.9 29.7 4 .5 7.1 3.9 7.1 7.9v-.1z' />
+                <Path d='M61 75H0V0h61c0 4.1-3 6.5-7 7-14.6 2.1-26 15.3-26 30.5S39.4 66 54 68c4 .5 7 3 7 7z' />
             </Svg>
         );
     }
@@ -15,8 +15,8 @@ const LeftShape = memo(
 const BottomShape = memo(
     (props: SvgProps): JSX.Element => {
         return (
-            <Svg width={75} height={61} viewBox='0 0 75 61' {...props}>
-                <Path d='M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z' />
+            <Svg width={74} height={61} viewBox='0 0 74 61' {...props}>
+                <Path d='M74 0v61H0V0c4.1 0 6.5 3 7 7 2.1 14.6 14.8 26 30 26S65 21.6 67 7c.5-4 3-7 7-7z' />
             </Svg>
         );
     }
@@ -26,7 +26,7 @@ const RightShape = memo(
     (props: SvgProps): JSX.Element => {
         return (
             <Svg width={61} height={75} viewBox='0 0 61 75' {...props}>
-                <Path d='M0 .1h61v75.2H0c0-4.1 3.1-7.4 7.1-7.9C21.7 65.3 33 52.8 33 37.6 33 22.4 21.7 9.9 7.1 7.9 3.1 7.4 0 4 0 0v.1z' />
+                <Path d='M0 0h61v75H0c0-4.1 3-6.5 7-7 14.6-2.1 26-15.3 26-30.5S21.6 9 7 7c-4-.5-7-3-7-7z' />
             </Svg>
         );
     }
