2c29bae4b7270fff7dbb1dc6be07906f8203ce03
RVFractal <rubenvillegas@fractal.software>
2021-07-05 11:52:56 -0500

    New build

--- dist/components/containers/index.js
+++ dist/components/containers/index.js
@@ -1,12 +1,13 @@
 export * from './ImageBackground';
 export * from './ImageBackground/types';
-export * from './SafeAreaLayer';
 export * from './Layer';
 export * from './Layer/types';
+export * from './SafeAreaLayer';
+export * from './ScrollView';
 export * from './Background';
 export * from './Box';
+export * from './HorizontalLayer';
+export * from './LoadingBackground';
 export * from './MarginLayer';
 export * from './PaddingLayer';
-export * from './HorizontalLayer';
-export * from './ScrollView';
 //# sourceMappingURL=index.js.map
